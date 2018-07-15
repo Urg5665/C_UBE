00189b10db52362e4269d46cf934032621d43e32
Carbonphry <hunterwustation@gmail.com>
2018-07-15 16:02:55 -0400

    Movement and Camera

---
+++ Wu_Hunter_Assignment_05/Assets/Scripts/MushController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MushController : MonoBehaviour {
+
+	private Animator anim;
+	public int health = 900;
+
+	// Use this for initialization
+	void Start () {
+
+		anim = GetComponent<Animator> ();
+	}
+	
+	public void DisableMush ()
+	{
+		gameObject.SetActive (false);
+	}
+}
