41cda6a54f7bc7dfaaecacaf3fc4fce79ba02381
Urg5665 <39248834+Urg5665@users.noreply.github.com>
2018-07-27 17:33:11 -0400

    5 30 push

---
+++ C_UBE Unity/Assets/Wu_Hunter_Assignment_05/Assets/Scripts/CharGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharGenerator : MonoBehaviour {
+
+    // Mark S
+    // attach this to a given char pre fab to assign them a random character
+
+    public bool agitated; //  Calm
+    public bool confused; // Inspire
+    public bool angry; // Pressure
+
+    // Use this for initialization
+    void Start () {
+        agitated = (Random.value > 0.5f);
+        confused = (Random.value > 0.5f);
+        angry = (Random.value > 0.5f);
+
+        //Debug.Log(agitated);
+        //Debug.Log(confused);
+        //Debug.Log(angry);
+
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+
+}
---
+++ C_UBE Unity/Assets/Wu_Hunter_Assignment_05/Assets/Scripts/DialogueGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueGenerator : MonoBehaviour {
+    //    // Mark S  7_27
+    // takes in characetr an runs trhough dialogue options
+
+    public GameObject person;
+
+
+
+    // Use this for initialization
+    void Start () {
+
+        //Debug.Log(person.agitated);
+        //Debug.Log(person.confused);
+        //Debug.Log(person.angry);
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+
+
+
+
+        
+
+
+
+
+    }
+}
